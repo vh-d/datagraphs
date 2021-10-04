1798384cfc9ddc617c31e48022bca042a307f012
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2021-10-04 20:47:13 +0200

    Pass to igraph methods.

--- R/igraph.R
+++ R/igraph.R
@@ -1,2 +1,20 @@
 #' @export
 are_adjacent.igraph <- igraph::are_adjacent
+
+#' @export
+add_edges.igraph <- igraph::add_edges
+
+#' @export
+add_vertices.igraph <- igraph::add_vertices
+
+#' @export
+neighborhood.igraph <- igraph::neighborhood
+
+#' @export
+neighbors.igraph <- igraph::neighbors
+
+#' @export
+E.igraph <- igraph::E
+
+#' @export
+V.igraph <- igraph::V
